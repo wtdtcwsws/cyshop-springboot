373931ee2ea79a54cb8ec686042c577b5a4087db
luohai <541545580@qq.com>
2019-11-13 17:57:28 +0800

    11-12：购物车功能实现-罗海

--- src/main/java/com/cy/cyshopspringboot/web/CartController.java
+++ src/main/java/com/cy/cyshopspringboot/web/CartController.java
@@ -1,7 +1,16 @@
 package com.cy.cyshopspringboot.web;
 
+import com.cy.cyshopspringboot.domain.CartMessage;
+import com.cy.cyshopspringboot.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import reactor.core.publisher.Mono;
+
+import javax.xml.ws.Response;
+import java.util.List;
 
 /**
  * @ClassName: CartController
@@ -12,22 +21,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CartController {
 
+    @Autowired
+    private CartService cartService;
 
     @RequestMapping("/cart")
-    public String cart(){
-        return "cart";
-    }
+    public Mono<String> cart(Model model) {
 
-    @RequestMapping("/head")
-    public String head(){
-        return "head";
-    }
+        List<CartMessage> cartMessages = cartService.cartMessageAll(2);
 
-    @RequestMapping("/foot")
-    public String foot(){
-        return "foot";
-    }
+        model.addAttribute("gouwuche",cartMessages);
 
+        return Mono.create(indexMono->indexMono.success("cart"));
+    }
 
 
 }
